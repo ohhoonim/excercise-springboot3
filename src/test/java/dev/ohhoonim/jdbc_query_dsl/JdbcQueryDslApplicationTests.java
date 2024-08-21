88ee42787b7153c5202634a9e9f6b25d44d90f9e
mathew <donnj95@gmail.com>
2024-08-22 06:51:47 +0900

    modulith verify

--- src/test/java/dev/ohhoonim/jdbc_query_dsl/JdbcQueryDslApplicationTests.java
+++ src/test/java/dev/ohhoonim/jdbc_query_dsl/JdbcQueryDslApplicationTests.java
@@ -2,12 +2,18 @@ package dev.ohhoonim.jdbc_query_dsl;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
 
 @SpringBootTest
 class JdbcQueryDslApplicationTests {
 
 	@Test
 	void contextLoads() {
+		var modules = ApplicationModules.of(JdbcQueryDslApplication.class);
+		for (var m : modules) {
+			System.out.println(m.getName() + " : " + m.getBasePackage());
+		}
+		modules.verify();
 	}
 
 }

a0c611bcf749a35da082d5a42949e0e708f10a5a
주형돈 <hdju@rntime.com>
2024-10-13 11:51:41 +0900

    .env 적용 Fixes #13

--- src/main/java/dev/ohhoonim/jdbc_query_dsl/domain/inventories/service/ProductApi.java
+++ src/main/java/dev/ohhoonim/jdbc_query_dsl/domain/inventories/service/ProductApi.java
@@ -1,8 +1,5 @@
 package dev.ohhoonim.jdbc_query_dsl.domain.inventories.service;
 
-import java.util.Optional;
-
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
